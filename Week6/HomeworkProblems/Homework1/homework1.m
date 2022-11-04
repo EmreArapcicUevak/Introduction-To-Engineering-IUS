e26209687156492f659693f3e97153c1ebde7ad4
Emre ArapcicUvak <EmreArapcicUevak@gmail.com>
2022-11-04 15:52:18 +0100

    Finish work on problem homework1 for week 6

---
+++ Week6/HomeworkProblems/Homework1/homework1.m
@@ -0,0 +1,4 @@
+printf("For rolls [5 1 4 6 5 5 6 6 5 2] you get %d money\n", diceGame([5 1 4 6 5 5 6 6 5 2]));
+printf("For rolls [2 4 1 3 6 6 6 4 5 3] you get %d money\n", diceGame([2 4 1 3 6 6 6 4 5 3]));
+printf("For rolls [1 4 3 2 5 3 4 2 6 5] you get %d money\n", diceGame([1 4 3 2 5 3 4 2 6 5]));
+
