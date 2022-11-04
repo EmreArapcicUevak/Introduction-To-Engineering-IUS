27921fee68b6b64ba0856e9138ba0a724ac63b8e
Emre ArapcicUvak <EmreArapcicUevak@gmail.com>
2022-11-04 11:20:35 +0100

    Fix small issue with problem tutorial3 for week 6

--- Week6/tutorial3/polygon.m
+++ Week6/tutorial3/polygon.m
@@ -3,7 +3,7 @@ function [] = polygon(n)
     n = 5;
   endif
 
-  angle = linspace(0, 2*pi,n);
-  radius = ones(1, n);
+  angle = linspace(0, 2*pi,n+1);
+  radius = ones(1, n+1);
   polar(angle,radius);
 endfunction
