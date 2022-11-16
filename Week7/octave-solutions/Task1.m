a054b80fd49750e505fb1be8c2209de0eebea636
Vedad <siljicvedad13@gmail.com>
2022-11-16 18:20:45 +0100

    Finish work on problem 1.3 for octave problems for homework7

--- Week7/octave-solutions/Task1.m
+++ Week7/octave-solutions/Task1.m
@@ -3,3 +3,4 @@ ProdNames = {"NpN103Trans", "MCchip3001", "IUScriptCore"}
 Prod = [2007 68 410 96; 2008 0 0 0; 2009 10 9582 9999; 2010 6402 10 400]
 totalProd = [length(Prod(:, 1)), sum(sum(Prod(:,2:end)))]
 avgProd = [length(Prod(:, 1)), mean(Prod(:,2:end))]
+yearProd = [Prod(:, 1), sum(Prod(:,2:end)')']
